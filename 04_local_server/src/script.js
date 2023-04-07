508dc32428ac9f07c4552663a06ba5515f816024
Alejandro Zapata <alejo8708@gmail.com>
2023-04-07 18:15:39 +0200

    04_local_server-end

---
+++ 04_local_server/src/script.js
@@ -0,0 +1,32 @@
+import * as THREE from "three"
+
+const scene = new THREE.Scene()
+
+//Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+//Red Cube
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color: 0xff0000})
+const cubeMesh = new THREE.Mesh(geometry, material)
+scene.add(cubeMesh)
+
+//Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+
+
+//Renderer
+const canvas = document.querySelector(".webglCanvas1")
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
