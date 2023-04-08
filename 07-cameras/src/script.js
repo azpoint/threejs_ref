1164841a58f436dab36b13215801007846734df5
Alejandro Zapata <alejo8708@gmail.com>
2023-04-08 19:11:05 +0200

    07-end

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -1,4 +1,18 @@
 import * as THREE from 'three'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+
+
+// Cursor
+const cursor = {
+    x: 0,
+    y: 0
+}
+
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = -(event.clientY / sizes.height - 0.5)
+
+})
 
 /**
  * Base
@@ -23,12 +37,25 @@ const mesh = new THREE.Mesh(
 scene.add(mesh)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
-camera.lookAt(mesh.position)
+const aspectRatio = sizes.width / sizes.height
+const camera = new THREE.PerspectiveCamera(60, aspectRatio, 0.1, 100)
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100 )
+
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 3
+// camera.lookAt(mesh.position)
 scene.add(camera)
+
+//Controls
+
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+// controls.target.y = -1
+// controls.update()
+
+
+
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -39,18 +66,24 @@ renderer.setSize(sizes.width, sizes.height)
 // Animate
 const clock = new THREE.Clock()
 
-const tick = () =>
+const frame = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
-    // Update objects
-    mesh.rotation.y = elapsedTime;
+    // Update camera
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2
+    // camera.position.y = cursor.y * 3
+    // camera.lookAt(mesh.position)
 
-    // Render
+    // Update Controls
+    controls.update()
+
+// Render
     renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+    // Call frame again on the next frame
+    window.requestAnimationFrame(frame)
 }
 
-tick()
+frame()
